8d6d7e9cd28fde086dbe30ee015ec45b475aa796
Rishav Sharma <sharmarishav676@gmail.com>
2025-03-26 18:17:45 +0530

    changes for deployment

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1424', // Replace with your MySQL password
-    database: 'school_management'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const schoolRoutes = require('./routes/schoolRoutes');
-
 const app = express();
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
